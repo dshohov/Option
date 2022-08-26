9b2d841e58a2675e79a0dff06ed394c3df36b1c9
Dmitry Shokhov <dshohov@gmail.com>
2022-08-26 20:54:23 +0300

    add controller

---
+++ OptionWebApplication/Controllers/AssemblyController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OptionWebApplication.Controllers
+{
+    public class AssemblyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();  
+        }
+    }
+}
