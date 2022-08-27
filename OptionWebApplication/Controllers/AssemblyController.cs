8ad8fb5e98749e65d664a1ded7ae6bc89578b446
Dmitry Shokhov <dshohov@gmail.com>
2022-08-27 16:09:31 +0300

    Views Assembly and Guarentee

--- OptionWebApplication/Controllers/AssemblyController.cs
+++ OptionWebApplication/Controllers/AssemblyController.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OptionWebApplication.Data;
+using OptionWebApplication.Models;
 
 namespace OptionWebApplication.Controllers
 {
     public class AssemblyController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public AssemblyController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();  
+            List<Assembly> assembly = _context.Assemblies.ToList();
+            return View(assembly);  
         }
     }
 }
--- OptionWebApplication/Controllers/GuarenteeController.cs
+++ OptionWebApplication/Controllers/GuarenteeController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OptionWebApplication.Data;
+using OptionWebApplication.Models;
 
 namespace OptionWebApplication.Controllers
 {
     public class GuarenteeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public GuarenteeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Guarentee> guarentees = _context.Guarentes.ToList();
+            return View(guarentees);
         }
     }
 }
