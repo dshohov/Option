b1cee89cbadd10f3fceefe037c349a26b4979b97
Dmitry Shokhov <dshohov@gmail.com>
2022-08-28 03:34:00 +0300

    Add funcion Add new Assembly and Add new Guarentee

--- OptionWebApplication/Controllers/AssemblyController.cs
+++ OptionWebApplication/Controllers/AssemblyController.cs
@@ -16,23 +16,40 @@ namespace OptionWebApplication.Controllers
             _context = context;
             _assemblyRepository = assemblyRepository;
         }
-
+        //Assembly Home Page(Главная страница сборки)
         public async Task<IActionResult> Index()
         {
             IEnumerable<Assembly> assembly = await _assemblyRepository.GetAll();
             return View(assembly);
         }
-
+        //View details(Посмотреть детально)
         public async Task<IActionResult> Detail(int id)
         {
             Assembly assembly = await _assemblyRepository.GetByIdAsync(id);
             return View(assembly);
         }
-
+        //Search within an assembly(Поиск внутри сборки)
         public async Task<IActionResult> DetailsBySerialNumber(int serialnumber)
         {
             Assembly assemblybyserialnumber = await _assemblyRepository.GetAssemblyBySerialNumber(serialnumber);
             return View(assemblybyserialnumber);
         }
+        //Go to creation page(Переход к странице создания)
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //Post request to the server to create a new class object Assembly(Post запрос на сервер с целью создания нового обьекта класса сборки)
+        [HttpPost]
+        public async Task<IActionResult> Create(Assembly assembly)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(assembly);
+            }
+            _assemblyRepository.Add(assembly);
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- OptionWebApplication/Controllers/GuarenteeController.cs
+++ OptionWebApplication/Controllers/GuarenteeController.cs
@@ -15,20 +15,39 @@ namespace OptionWebApplication.Controllers
             _guarenteeRepository = guarenteeRepository;
 
         }
+        //Guarentee Home Page(Главная страница гарантии)
         public IActionResult Index()
         {
             List<Guarentee> guarentees = _context.Guarentes.ToList();
             return View(guarentees);
         }
+        //View details(Посмотреть детально)
         public IActionResult Detail(int id)
         {
             Guarentee guarentee = _context.Guarentes.FirstOrDefault(c => c.Id == id);
             return View(guarentee);
         }
+        //Search within an Guarentee(Поиск внутри гарантии)
         public async Task<IActionResult> DetailsBySerialNumber(int serialnumber)
         {
             Guarentee guarenteebyserialnumber = await _guarenteeRepository.GetGuarenteeBySerialNumber(serialnumber);
             return View(guarenteebyserialnumber);
+        }
+        //Go to creation page(Переход к странице создания)
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //Post request to the server to create a new class object Assembly(Post запрос на сервер с целью создания нового обьекта класса сборки)
+        [HttpPost]
+        public async Task<IActionResult> Create(Guarentee guarentee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(guarentee);
+            }
+            _guarenteeRepository.Add(guarentee);
+            return RedirectToAction("Index");
         }
     }
 }
